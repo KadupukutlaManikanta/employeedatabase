6990f4e070a696c29f96201cbf25234ad8bbe176
manikanta <manikantakadupukutla@gmail.com>
2024-03-09 18:22:32 +0530

    adding project to repo

---
+++ salary/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import get_object_or_404, render
+
+from salary.models import Employee
+
+def employee_details(request,pk):
+     employee=get_object_or_404(Employee,pk=pk)
+     context={
+          'employee':employee,
+     }
+     return render(request,'employee_details.html',context)
